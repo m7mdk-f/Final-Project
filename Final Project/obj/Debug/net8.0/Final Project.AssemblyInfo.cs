e4d7958a3d923b589c9f39ca31a0e51b0808efb5
Mohamed Farjallah <132541340+m7mdk-f@users.noreply.github.com>
2025-04-05 17:28:28 +0300

    .

--- Final Project/obj/Debug/net8.0/Final Project.AssemblyInfo.cs
+++ Final Project/obj/Debug/net8.0/Final Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc2fd8d32323062bbf0aa4c148c1a5e73a8668d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bae20046e197ea33537e98856cb0e00fd0f1de9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
